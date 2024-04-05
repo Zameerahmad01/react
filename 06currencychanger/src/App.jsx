5e1176e892db50f199b8236fd95dee2bf9473215
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-04-05 23:19:25 +0500

    change background image currency project

--- 06currencychanger/src/App.jsx
+++ 06currencychanger/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
         style={{
-            backgroundImage: `url('https://images.pexels.com/photos/210574/pexels-photo-210574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
+            backgroundImage: `url('https://images.pexels.com/photos/534216/pexels-photo-534216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
         }}
     >
         <div className="w-full">
@@ -76,6 +76,7 @@ function App() {
                               setTo(currency)
                             }
                           }
+                          
 
                         />
                     </div>
